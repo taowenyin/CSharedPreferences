c7e80829b53f43624ea214333dd1ec2ad269a831
陶文寅 <wenyin.tao@163.com>
2016-08-21 21:23:29 +0800

    添加代码接口框架

---
+++ CSharedPreferences/IEditor.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharedPreferences
+{
+    public interface IEditor
+    {
+        /// <summary>
+        /// 清空所有Key值
+        /// </summary>
+        /// <returns>IEditor对象</returns>
+        IEditor Clear();
+
+        /// <summary>
+        /// 提交所有值
+        /// </summary>
+        /// <returns>IEditor对象</returns>
+        IEditor Commit();
+
+        /// <summary>
+        /// 向SharesPreference添加Boolean类型的值
+        /// </summary>
+        /// <param name="key">Boolean类型的Key</param>
+        /// <param name="value">Boolean类型的值</param>
+        /// <returns>IEditor对象</returns>
+        IEditor PutBoolean(string key, bool value);
+
+        /// <summary>
+        /// 向SharesPreference添加Float类型的值
+        /// </summary>
+        /// <param name="key">Float类型的Key</param>
+        /// <param name="value">Float类型的值</param>
+        /// <returns>IEditor对象</returns>
+        IEditor PutFloat(string key, float value);
+
+        /// <summary>
+        /// 向SharesPreference添加Int类型的值
+        /// </summary>
+        /// <param name="key">Int类型的Key</param>
+        /// <param name="value">Int类型的值</param>
+        /// <returns>IEditor对象</returns>
+        IEditor PutInt(string key, int value);
+
+        /// <summary>
+        /// 向SharesPreference添加Long类型的值
+        /// </summary>
+        /// <param name="key">Long类型的Key</param>
+        /// <param name="value">Long类型的值</param>
+        /// <returns>IEditor对象</returns>
+        IEditor PutLong(string key, long value);
+
+        /// <summary>
+        /// 向SharesPreference添加String类型的值
+        /// </summary>
+        /// <param name="key">String类型的Key</param>
+        /// <param name="value">String类型的值</param>
+        /// <returns>IEditor对象</returns>
+        IEditor PutString(string key, string value);
+
+        /// <summary>
+        /// 移除对应Key的值
+        /// </summary>
+        /// <param name="key">要移除的Key</param>
+        /// <returns>IEditor对象</returns>
+        IEditor Remove(string key);
+    }
+}
---
+++ CSharedPreferences/SharedPreferences.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharedPreferences
+{
+    public class SharedPreferences
+    {
+        private static SharedPreferences instance = null;
+        private string sharedPrefName = null;
+
+        private SharedPreferences(string name)
+        {
+            this.sharedPrefName = name;
+        }
+
+        /// <summary>
+        /// 获取SharedPreferences单例
+        /// </summary>
+        /// <param name="name">SharedPreferences文件名</param>
+        /// <returns>SharedPreferences单例对象</returns>
+        public static SharedPreferences GetInstance(string name)
+        {
+            if(instance == null || !instance.getSharedPrefName().Equals(name))
+            {
+                instance = new SharedPreferences(name);
+            }
+
+            return instance;
+        }
+
+        /// <summary>
+        /// 判断Key值是否存在
+        /// </summary>
+        /// <param name="key">要判断的Key值</param>
+        /// <returns>Ture：表示Key存在，False：表示Key不存在</returns>
+        public bool Contains(string key)
+        {
+            return false;
+        }
+
+        /// <summary>
+        /// 获取Editor对象
+        /// </summary>
+        /// <returns>Editor对象</returns>
+        public IEditor GetEditor()
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// 获取Boolean类型的Key值
+        /// </summary>
+        /// <param name="key">检索的Key值</param>
+        /// <param name="defValue">如果Key不存在，则使用的默认值</param>
+        /// <returns>Key对应的值</returns>
+        public bool GetBoolean(string key, bool defValue)
+        {
+            return defValue;
+        }
+
+        /// <summary>
+        /// 获取Float类型的Key值
+        /// </summary>
+        /// <param name="key">检索的Key值</param>
+        /// <param name="defValue">如果Key不存在，则使用的默认值</param>
+        /// <returns>Key对应的值</returns>
+        public float GetFloat(string key, float defValue)
+        {
+            return defValue;
+        }
+
+        /// <summary>
+        /// 获取Int类型的Key值
+        /// </summary>
+        /// <param name="key">检索的Key值</param>
+        /// <param name="defValue">如果Key不存在，则使用的默认值</param>
+        /// <returns>Key对应的值</returns>
+        public int GetInt(string key, int defValue)
+        {
+            return defValue;
+        }
+
+        /// <summary>
+        /// 获取Long类型的Key值
+        /// </summary>
+        /// <param name="key">检索的Key值</param>
+        /// <param name="defValue">如果Key不存在，则使用的默认值</param>
+        /// <returns>Key对应的值</returns>
+        public long GetLong(string key, long defValue)
+        {
+            return defValue;
+        }
+
+        /// <summary>
+        /// 获取String类型的Key值
+        /// </summary>
+        /// <param name="key">检索的Key值</param>
+        /// <param name="defValue">如果Key不存在，则使用的默认值</param>
+        /// <returns>Key对应的值</returns>
+        public string GetString(string key, string defValue)
+        {
+            return defValue;
+        }
+
+        /// <summary>
+        /// 获取SharedPreferences文件名
+        /// </summary>
+        /// <returns>SharedPreferences文件名</returns>
+        private string getSharedPrefName()
+        {
+            return this.sharedPrefName;
+        }
+    }
+}
